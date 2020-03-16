a7e7a161877c15bf4f0a93be6bf8995fb59f19a6
mynameisjenn <jenniferlynnhatton@gmail.com>
2020-03-15 19:10:30 -0500

    added signin component, needs spacing fixes, axios and tailwind

--- recordstore-frontend/src/main.js
+++ recordstore-frontend/src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
 import router from './router'
 
 Vue.config.productionTip = false
--- recordstore-frontend/src/router/index.js
+++ recordstore-frontend/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Signin from '@/components/Signin.vue'
+// Vue.use(VueAxios, axios)
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',	
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Signin',
+      component: Signin
     }
   ]
 })
